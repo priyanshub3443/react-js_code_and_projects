af1d8775ea1760be8c945cec76e65c809c02631b
priyanshu bharti <110742345+priyanshub3443@users.noreply.github.com>
2025-11-01 23:59:16 +0530

    starting

---
+++ starting/src/button.jsx
@@ -0,0 +1,5 @@
+function Pbutton() {
+  return <button> Its me buddy</button>
+}
+
+export default Pbutton;
---
+++ starting/src/hello.jsx
@@ -0,0 +1,8 @@
+function Hello(){
+    let myName = 'Priyanshu';
+  return <h3>
+    Hello This is the future of speaking. I am your master {myName}.
+  </h3>
+}
+
+export default Hello;
