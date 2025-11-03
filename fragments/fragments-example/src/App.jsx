6328f5993f1d0fa5e120b42f1241ddcf427a830e
priyanshu bharti <110742345+priyanshub3443@users.noreply.github.com>
2025-11-04 00:31:46 +0530

    fragment

--- fragments/fragments-example/src/App.jsx
+++ fragments/fragments-example/src/App.jsx
@@ -1,11 +1,20 @@
 
 import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
-
+  let foodItems = ["Dal", "Green Vegetable", "Roti", "Salad", "Milk"];
 
   return (
-    <div>Fragment</div>
+    <>
+      <h1>Healthy Food</h1>
+      <ul class="list-group">
+        {foodItems.map((item) => (<li class="list-group-item">{item}</li>))
+        }
+
+
+      </ul>
+    </>
   )
 }
 
