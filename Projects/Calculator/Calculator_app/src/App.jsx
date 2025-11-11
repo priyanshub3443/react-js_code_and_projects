d36ed0957725755ad779f4fe90f99aaeec47de85
priyanshu bharti <110742345+priyanshub3443@users.noreply.github.com>
2025-11-11 21:42:40 +0530

    complet calculator app

--- Projects/Calculator/Calculator_app/src/App.jsx
+++ Projects/Calculator/Calculator_app/src/App.jsx
@@ -1,19 +1,32 @@
-import Display from './components/display';
-import styles from './App.module.css';
-import ButtonsContainer from './components/ButtonsContainer';
-
+import Display from "./components/Display";
+import ButtonsContainer from "./components/ButtonsContainer";
+import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [calVal, setCalVal] = useState("");
 
+  const onButtonClick = (buttonText) => {
+    if (buttonText === "C") {
+      setCalVal("");
+    } else if (buttonText === "=") {
+      const result = eval(calVal);
+      setCalVal(result);
+    } else {
+      const newDisplayValue = calVal + buttonText;
+      setCalVal(newDisplayValue);
+    }
+  };
 
   return (
+    <center>
 
-    <div className={styles.calculator}>
-      <Display></Display>
-      <ButtonsContainer></ButtonsContainer>
-    </div>
-
-  )
+      <div className={styles.calculator}>
+        <Display displayValue={calVal}></Display>
+        <ButtonsContainer onButtonClick={onButtonClick}></ButtonsContainer>
+      </div>
+    </center>
+  );
 }
 
-export default App
+export default App;
