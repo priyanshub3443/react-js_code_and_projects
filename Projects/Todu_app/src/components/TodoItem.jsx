095b5840644519b73667d9874ee4334d2be47d7c
priyanshu bharti <110742345+priyanshub3443@users.noreply.github.com>
2025-11-11 22:49:22 +0530

    completed Todo App

--- Projects/Todu_app/src/components/TodoItem.jsx
+++ Projects/Todu_app/src/components/TodoItem.jsx
@@ -1,4 +1,4 @@
-function TodoItem({ todoName, todoDate }) {
+function TodoItem({ todoName, todoDate, onDeleteClick }) {
 
 
   return (
@@ -7,7 +7,7 @@ function TodoItem({ todoName, todoDate }) {
         <div className="col-6">{todoName}</div>
         <div className="col-4">{todoDate}</div>
         <div className="col-2">
-          <button type="button" className="btn btn-danger kg-button">
+          <button type="button" className="btn btn-danger kg-button" onClick={() => onDeleteClick(todoName)}>
             Delete
           </button>
         </div>
--- Projects/Todu_app/src/components/TodoItems.jsx
+++ Projects/Todu_app/src/components/TodoItems.jsx
@@ -1,10 +1,10 @@
 import TodoItem from "./TodoItem";
 
-const TodoItems = ({ todoItems }) => {
+const TodoItems = ({ todoItems, onDeleteClick }) => {
   return (
     <div className="items-container">
       {todoItems.map((item) => (
-        <TodoItem todoDate={item.dueDate} todoName={item.name}></TodoItem>
+        <TodoItem todoDate={item.dueDate} todoName={item.name} onDeleteClick={onDeleteClick}></TodoItem>
       ))}
 
 
